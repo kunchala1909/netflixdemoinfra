051191a543cff2a084fc1b4a99fd04c3570b38ea
kunchala1909 <144514521+kunchala1909@users.noreply.github.com>
2025-10-08 12:32:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "aws_instance" "one" {
 count = 4
 ami = "ami-02d26659fd82cf299"
 instance_type = "t2.medium"
-key_name = "demoproject1"
-vpc_security_group_ids = ["sg-0cdd4aaa4687f110c"]
+key_name = "TERRAFORMKEYPAIR"
+vpc_security_group_ids = ["vpc-0d7a67f09e82653b5"]
 tags = {
 Name = var.instance_names[count.index]
 }
