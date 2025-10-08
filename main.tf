d4acd052e53953e4af6d417f9377dd72002d36e0
kunchala1909 <144514521+kunchala1909@users.noreply.github.com>
2025-10-08 15:11:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ region = "ap-south-1"
 resource "aws_instance" "one" {
 count = 4
 ami = "ami-02d26659fd82cf299"
-instance_type = "t2.medium"
+instance_type = "t2.micro"
 key_name = "terraformkey"
 vpc_security_group_ids = ["sg-01414cb271b3058e5"]
 tags = {
