6a35bdf0b7235a5b2e2df8ead3c066a6fb577d99
kunchala1909 <144514521+kunchala1909@users.noreply.github.com>
2025-10-08 15:01:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-region = "ap-south-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
-count = 4
-ami = "ami-02d26659fd82cf299"
-instance_type = "t2.medium"
-key_name = "TERRAFORMKEYPAIR"
-vpc_security_group_ids = ["sg-098d6c6e8023f63eb"]
-tags = {
-Name = var.instance_names[count.index]
-}
+  ami                    = "ami-032659fb89df2f299"
+  instance_type          = "t2.micro"       
+  key_name               = "TERRAFORMKEYPAIR"
+  vpc_security_group_ids = ["sg-095d6c689e23f63eb"]  
+
+  tags = {
+    Name = "Jenkins"  
+  }
 }
 
 variable "instance_names" {
-default = ["jenkins", "AppServer-1", "AppServer-2", "Monitoring server"]
+  default = ["Jenkins", "AppServer-1", "AppServer-2", "Monitoring server"]
 }
